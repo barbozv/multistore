9def471ea51863fe720baca1a6dc92394af880a9
bz <barbosaaa_@outlook.com>
2022-11-25 19:36:00 -0300

    feat: define a font padrao

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import { GlobalStyles } from '../styles/globals'
 import type { AppProps } from 'next/app'
+import { Montserrat } from '@next/font/google'
+const montserrat = Montserrat({ subsets: ['latin'], weight: '400' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className} >
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- styles/globals.ts
+++ styles/globals.ts
@@ -1,14 +1,14 @@
-html,
-body {
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+
+  html,
+  body {
   padding: 0;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
+    
 }
 
 * {
@@ -23,4 +23,4 @@ a {
     color: white;
     background: black;
   }
-}
+`
